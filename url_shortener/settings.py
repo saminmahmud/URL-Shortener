2783f49074969419324d8eac746b6f22a3888dea
saminmahmud <samin.mahmud18@gmail.com>
2024-05-16 18:45:21 +0600

    CSRF added

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -30,7 +30,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://mamarbank-lbnt.onrender.com','https://*.127.0.0.1','http://127.0.0.1:8000/']
+CSRF_TRUSTED_ORIGINS = ['https://url-shortener-7msx.onrender.com','https://*.127.0.0.1','http://127.0.0.1:8000/']
 # Application definition
 
 INSTALLED_APPS = [
